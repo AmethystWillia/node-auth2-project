04d036091d20203e8707dc21d78881c282d4860c
Amethyst Willia <zmithic@gmail.com>
2022-02-22 16:14:41 -0600

    Messed around with login but it isn't working

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
@@ -37,6 +39,31 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  const { username, password } = req.body;
+
+  username.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({ message: `${user.username} is back!`, token })
+      } else {
+        res.status()
+      }
+    })
 });
+
+const generateToken = (user) => {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role: user.role_name,
+  };
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, JWT_SECRET, options);
+}
 
 module.exports = router;
